9542fb415820ee6c5c0960dfcde4f29484d824b9
AlexGheorghe <galex.marius@gmail.com>
2021-05-28 13:00:00 +0300

    Incercare rezolvare punct 9

--- antrenareDat/main.py
+++ antrenareDat/main.py
@@ -5,8 +5,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+import torchvision
+import matplotlib.pyplot as plt
+import numpy as np
+from torchinfo import summary
 
-# Generarea retelei neurale
+## Retea initiala
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -25,7 +30,32 @@ class Net(nn.Module):
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
 
-# Functia de antrenare a retelei neurale
+# Generarea retelei neurale
+# class Net(nn.Module):
+#     # Definirea straturilor retelei
+#     def __init__(self):
+#         super(Net, self).__init__()
+#         self.conv1 = nn.Conv2d(in_channels = 1, out_channels = 20, kernel_size = 5, stride = 1)
+#         self.conv2 = nn.Conv2d(in_channels = 20, out_channels = 50, kernel_size = 3, stride = 1)
+#         self.conv3 = nn.Conv2d(in_channels= 50, out_channels = 100, kernel_size = 2, stride=1)
+#         self.fc1 = nn.Linear(in_features = 4 * 4 * 100, out_features = 500)
+#         self.fc2 = nn.Linear(in_features = 500, out_features = 10)
+#
+#     # Definirea filtrelor si modul de conectare intre layere
+#     def forward(self, x):
+#         x = F.relu(self.conv1(x))
+#         x = F.max_pool2d(x, 2, 2)
+#         x = F.relu(self.conv2(x))
+#         x = F.max_pool2d(x, 2, 2)
+#         x = F.relu(self.conv3(x))
+#         x = F.max_pool2d(x, 2, 2)
+#         print(x.shape)
+#         x = x.view(-1, 4 * 4 * 100)
+#         x = F.relu(self.fc1(x))
+#         x = self.fc2(x)
+#         return F.log_softmax(x, dim=1)
+
+###### Functia de antrenare data a retelei neurale
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
@@ -40,7 +70,7 @@ def train(args, model, device, train_loader, optimizer, epoch):
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                        100. * batch_idx / len(train_loader), loss.item()))
 
-# Functia de testare a retelie neurale
+###### Functia de testare a retelie neurale
 def test(args, model, device, test_loader):
     model.eval()
     test_loss = 0
@@ -60,13 +90,73 @@ def test(args, model, device, test_loader):
         100. * correct / len(test_loader.dataset)))
 
 
+###### Rezolvare punctul 9 ###### ! Practic nu ar trebui sa il folosesc
+def antrenamentNou(imgDB, epoch, criterion, optimizer):
+
+    running_loss = 0.0
+
+    for i, data in enumerate(imgDB):
+        inputs, labels = data
+        optimizer.zero_grad()
+
+        outputs = Net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # Afisare statistici
+        running_loss += loss.item()
+        if i % 2000 == 1999:  # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+        print('Finished Training')
+
+        # Salvarea retelei neurale
+        # torch.save(Net.state_dict(), PATH)
+
+
+###### Rezolvare punctul 9 #######
+
+# Cred ca as putea sa fac asa:
+# - mai fac un script nou de antrenament
+# - initializez 3 variabile cu imagini random din baza de date de validare
+# - fac un for loop prin care sa trec fiecare imagine si sa o afisez impreuna cu label-ul
+# - totul va fi trimis cu un iterator
+
+
+
+
+
+
+def imshow(img):
+        img = img / 2 + 0.5  # unnormalize
+        npimg = img.numpy()
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+        plt.show()
+
+def imageTrainShow(database, classes, batch_size):
+    for i, data, in enumerate(database):
+        dataiter = iter(database)
+        # get some random training images
+        images, labels = dataiter.next()
+        # show images
+        imshow(torchvision.utils.make_grid(images))
+        print(' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
+
+
+
+
 def main():
     # Training settings ? De ce a folosit asa sa transfere argumentele?
     # ?Cum functioneaza parser-ul? De ce nu au fost stocate in niste variabile ci au fost folosite argumente?
 
+    ## Argumente folosite pentru algoritmii de antrenare si testare date
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
     parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
+    parser.add_argument('--batch-size-img', type=int, default=3, metavar='N',
+                        help='input training images to show to user (default: 3)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=10, metavar='N',
@@ -93,7 +183,7 @@ def main():
 
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 
-    ## Preluare informatii pentru antrenament
+    ## Baza de date de antrenament
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../data', train=True, download=True,
                        transform=transforms.Compose([
@@ -102,7 +192,17 @@ def main():
                        ])),
                         batch_size=args.batch_size, shuffle=True, **kwargs)
 
-    ## Preluare informatii pentru testare
+    train_loader_img = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=3, shuffle=True, **kwargs)
+
+    classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+    ## Baza de date de validare
     test_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../data', train=False, transform=transforms.Compose([
             transforms.ToTensor(),
@@ -110,18 +210,26 @@ def main():
         ])),
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
-    model = Net().to(device) # transferul retelei catre placa video sau procesor
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    # Afisarea a 3 imagini de antrenament
+    # imageTrainShow()
 
-    for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(args, model, device, test_loader)
+    # Definire functie loss and optimizer pentru algoritmul nou de antrenare
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(Net.parameters(), lr=0.001, momentum=0.9)
+
+    model = Net().to(device) # transferul retelei catre placa video sau procesor
+    # optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+
+    # for epoch in range(1, args.epochs + 1):
+    #    train(args, model, device, train_loader, optimizer, epoch)
+    #    test(args, model, device, test_loader)
 
     if (args.save_model):
         torch.save(model.state_dict(), "mnist_cnn.pt")
 
-
 if __name__ == '__main__':
     main()
 
+    #network = Net()
+    #summary(network)
 # epoch cred ca semnifica generatia retelei neurale
