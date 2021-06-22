9379a75f59956e8646501b77bd162ff77030c464
AlexGheorghe <galex.marius@gmail.com>
2021-06-23 00:04:25 +0300

    Project work

--- antrenareDat/antrenament.py
+++ antrenareDat/antrenament.py
@@ -8,28 +8,8 @@ from torchvision import datasets, transforms
 import torchvision
 import matplotlib.pyplot as plt
 import numpy as np
-from torchinfo import summary
 
-#-------- Reteaua neurala primita --------#
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 20, 5, 1)
-        self.conv2 = nn.Conv2d(20, 50, 5, 1)
-        self.fc1 = nn.Linear(4 * 4 * 50, 500)
-        self.fc2 = nn.Linear(500, 10)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4 * 4 * 50)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
-
-# Incercare de retea neurala (structura momentan nefunctionala)
+# NN care merge, mai trebuie drop-out si batch
 class Net(nn.Module):
 
    def __init__(self):
@@ -37,7 +17,7 @@ class Net(nn.Module):
        self.conv1 = nn.Conv2d(in_channels = 1, out_channels = 20, kernel_size = 5, stride = 1)
        self.conv2 = nn.Conv2d(in_channels = 20, out_channels = 50, kernel_size = 3, stride = 1)
        self.conv3 = nn.Conv2d(in_channels= 50, out_channels = 100, kernel_size = 3, stride=1)
-       self.fc1 = nn.Linear(in_features = 8 * 8 * 100, out_features = 500)
+       self.fc1 = nn.Linear(in_features = 1 * 1 * 100, out_features = 500)
        self.fc2 = nn.Linear(in_features = 500, out_features = 10)
 
    def forward(self, x):
@@ -46,12 +26,14 @@ class Net(nn.Module):
        x = F.relu(self.conv2(x))
        x = F.max_pool2d(x, 2, 2)
        x = F.relu(self.conv3(x))
-       print(x.shape)
-       x = x.view(-1, 3 * 3 * 100)
+       x = F.max_pool2d(x, 2, 2)
+       x = x.view(-1, 1 * 1 * 100)
        x = F.relu(self.fc1(x))
        x = self.fc2(x)
 
        return F.log_softmax(x, dim=1)
+
+
 
 #-------- Functia de afisare a unei imagini --------#
 def imshow(img, title):
@@ -59,10 +41,6 @@ def imshow(img, title):
         standard = 0.3081
         # img = img / 2 + 0.5  # unnormalize
         img = (img * standard) + mean  # unnormalize
-
-        # 0.1307 mean
-        # 0.3081 standard
-
         npimg = img.numpy()
         plt.title(title)
         plt.imshow((np.transpose(npimg, (1, 2, 0))))
@@ -87,13 +65,6 @@ def train(args, model, device, train_loader, optimizer, epoch):
 
 #-------- Afisare imagini din baza de date ade antrenament  --------#
 def imageShow(database, title):
-    # for i, data, in enumerate(database):
-    #     dataiter = iter(database)
-    #     # get some random training images
-    #     images, labels = dataiter.next()
-    #     # show images
-    #     imshow(torchvision.utils.make_grid(images), title)
-    #     break
     dataiter = iter(database)
     images, labels = dataiter.next()
     imshow(torchvision.utils.make_grid(images[0:3]), title)
@@ -160,13 +131,13 @@ def main():
 
     #----------------- Rezolvare pct 7 ------------------#
     # Afisarea a 3 imagini din baza de date de antrenament
-    imageShowLabels(train_loader, "Training-images")
+    # imageShowLabels(train_loader, "Training-images")
     #----------------------------------------------------#
 
 
     # Antrenarea si testarea propriu-zisa a retelei neurale
-    # for epoch in range(1, args.epochs + 1):
-    #    train(args, model, device, train_loader, optimizer, epoch)
+    for epoch in range(1, args.epochs + 1):
+       train(args, model, device, train_loader, optimizer, epoch)
 
 
 
